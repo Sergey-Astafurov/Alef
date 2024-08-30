3f74d3b747c5c2225abfbe3130be1697f9079d91
Sergey-Astafurov <stupidsociety16@gmail.com>
2024-08-30 17:14:04 +0500

    авыа

--- alef-test-task/src/views/PreviewView.vue
+++ alef-test-task/src/views/PreviewView.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="contaner w-1/2 mx-auto">
     <h1>Персонльные данные</h1>
-    <div class="font-bold text-xl">
+    <div class="font-bold text-xl mb-20">
       {{ mainStore.name }}, {{ mainStore.age }} лет
     </div>
 
     <div v-if="mainStore.childrenList.length > 0">
-      <h3>Дети</h3>
-      <ul>
-        <li v-for="(children, index) in mainStore.childrenList" :key="index">
+      <h3 class="mb-4">Дети</h3>
+      <ul class="flex flex-col gap-4">
+        <li class="bg-gray-300 font-bold  p-3 w-64 " v-for="(children, index) in mainStore.childrenList" :key="index">
           {{ index+1 }}.
           {{ children.name == "" ? " Имя не указано " : children.name }},
           {{
